5d587dd876a82a8b4a6e2340ea0f2dd77609006a
Kai Kewley <kaikewley@gmail.com>
2014-05-30 14:08:14 +0100

    Col test using ValidationLog with unit test.

--- column-pattern-test/src/main/java/org/ihtsdo/release/assertion/ColumnPatternTest.java
+++ column-pattern-test/src/main/java/org/ihtsdo/release/assertion/ColumnPatternTest.java
@@ -1,8 +1,7 @@
 package org.ihtsdo.release.assertion;
 
 import org.ihtsdo.release.assertion._1_0.ColumnPatternTestConfiguration;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.ihtsdo.release.assertion.log.ValidationLog;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -20,6 +19,7 @@ import java.util.regex.PatternSyntaxException;
 // TODO: Unit Test this class thoroughly
 public class ColumnPatternTest {
 
+	private final ValidationLog validationLog;
 	private final ColumnPatternTestConfiguration configuration;
 	private final File rf2FilesDirectory;
 	private Map<String, Pattern> regexCache;
@@ -29,9 +29,9 @@ public class ColumnPatternTest {
 	private static final Pattern COMPONENT_ID_PATTERN = Pattern.compile("^\\d{6,18}$");
 
 	private static final String UTF_8 = "UTF-8";
-	private static final Logger LOGGER = LoggerFactory.getLogger(ColumnPatternTest.class);
 
-	public ColumnPatternTest(ColumnPatternTestConfiguration configuration, File rf2FilesDirectory) {
+	public ColumnPatternTest(ValidationLog validationLog, ColumnPatternTestConfiguration configuration, File rf2FilesDirectory) {
+		this.validationLog = validationLog;
 		this.configuration = configuration;
 		this.rf2FilesDirectory = rf2FilesDirectory;
 		regexCache = testConfigurationByPrecompilingRegexPatterns(configuration);
@@ -47,64 +47,70 @@ public class ColumnPatternTest {
 		// TODO: Report errors in a machine readable way for error organisation/navigation.
 		for (ColumnPatternTestConfiguration.File file : configuration.getFile()) {
 			String fileName = file.getName();
+			if (fileName != null) {
 
-			File rf2File = new File(rf2FilesDirectory, fileName);
-			if (rf2File.isFile()) {
-				try {
-					List<ColumnPatternTestConfiguration.File.Column> columns = file.getColumn();
-					int expectedColumnCount = columns.size();
-					BufferedReader reader = Files.newBufferedReader(rf2File.toPath(), Charset.forName(UTF_8));
-					filesTested++;
-					String line;
-					long lineNumber = 0;
+				File rf2File = new File(rf2FilesDirectory, fileName);
+				if (rf2File.isFile()) {
+					try {
+						List<ColumnPatternTestConfiguration.File.Column> columns = file.getColumn();
+						int expectedColumnCount = columns.size();
+						BufferedReader reader = Files.newBufferedReader(rf2File.toPath(), Charset.forName(UTF_8));
+						filesTested++;
+						String line;
+						long lineNumber = 0;
 
-					// Variables outside loop to force memory reuse.
-					String[] columnData;
-					String value;
+						// Variables outside loop to force memory reuse.
+						String[] columnData;
+						String value;
 
-					while ((line = reader.readLine()) != null) {
-						linesTested++;
-						lineNumber++;
-						columnData = line.split("\t");
-						int columnCount = columnData.length;
-						if (columnCount == expectedColumnCount) {
-							for (int columnIndex = 0; columnIndex < columnCount; columnIndex++) {
-								ColumnPatternTestConfiguration.File.Column column = columns.get(columnIndex);
-								value = columnData[columnIndex];
-								if (lineNumber == 1) {
-									// Test header value
-									testHeaderValue(value, column);
-								} else {
-									// Test data value
-									testDataValue(lineNumber, value, column);
+						while ((line = reader.readLine()) != null) {
+							linesTested++;
+							lineNumber++;
+							columnData = line.split("\t");
+							int columnCount = columnData.length;
+							if (columnCount == expectedColumnCount) {
+								for (int columnIndex = 0; columnIndex < columnCount; columnIndex++) {
+									ColumnPatternTestConfiguration.File.Column column = columns.get(columnIndex);
+									value = columnData[columnIndex];
+									if (lineNumber == 1) {
+										// Test header value
+										testHeaderValue(value, column);
+									} else {
+										// Test data value
+										testDataValue(lineNumber, value, column);
+									}
 								}
+							} else {
+								validationLog.assertionError("Column count on line {} does not match expectation: expected {}, actual {}", lineNumber, columnCount, expectedColumnCount);
 							}
-						} else {
-							LOGGER.error("Columns missing on line {}: expected {}, actual {}", lineNumber, columnCount, expectedColumnCount);
 						}
+					} catch (IOException e) {
+						validationLog.executionError("Problem reading file {}", fileName, e);
 					}
-				} catch (IOException e) {
-					LOGGER.error("Problem reading file {}", fileName, e);
+				} else {
+					validationLog.executionError("Unable to locate file {}", fileName);
 				}
 			} else {
-				LOGGER.error("Unable to locate file {}", fileName);
+				validationLog.configurationError("Filename is null.");
 			}
 		}
-		LOGGER.info("{} files and {} lines tested in {} milliseconds.", filesTested, linesTested, (new Date().getTime() - startTime.getTime()));
+
+		// TODO: Use startTest/endTest validation log methods instead.
+		validationLog.info("{} files and {} lines tested in {} milliseconds.", filesTested, linesTested, (new Date().getTime() - startTime.getTime()));
 	}
 
 	private void testDataValue(long lineNumber, String value, ColumnPatternTestConfiguration.File.Column column) {
 		// UUID
 		if (column.getUuid() != null) {
 			if (!UUID_PATTERN.matcher(value).matches()) {
-				LOGGER.error("Value does not match UUID pattern on line {}, column name '{}': value '{}'", lineNumber, column.getName(), value);
+				validationLog.assertionError("Value does not match UUID pattern on line {}, column name '{}': value '{}'", lineNumber, column.getName(), value);
 			}
 		}
 
 		// Boolean
 		if (column.getBoolean() != null) {
 			if (!"1".equals(value) && !"0".equals(value)) {
-				LOGGER.error("Value does not match Boolean pattern on line {}, column name '{}': value '{}'", lineNumber, column.getName(), value);
+				validationLog.assertionError("Value does not match Boolean pattern on line {}, column name '{}': value '{}'", lineNumber, column.getName(), value);
 			}
 		}
 
@@ -112,7 +118,7 @@ public class ColumnPatternTest {
 		String expectedValue = column.getValue();
 		if (expectedValue != null) {
 			if (!expectedValue.equals(value)) {
-				LOGGER.error("Value does not match Expected value on line {}, column name '{}': expected '{}', actual '{}'", lineNumber, column.getName(), expectedValue, value);
+				validationLog.assertionError("Value does not match Expected value on line {}, column name '{}': expected '{}', actual '{}'", lineNumber, column.getName(), expectedValue, value);
 			}
 		}
 
@@ -120,14 +126,14 @@ public class ColumnPatternTest {
 		ColumnPatternTestConfiguration.File.Column.DateStamp dateStamp = column.getDateStamp();
 		if (dateStamp != null) {
 			if (!DATE_PATTERN.matcher(value).matches()) {
-				LOGGER.error("Value does not match Date Stamp pattern on line {}, column name '{}': value '{}'", lineNumber, column.getName(), value);
+				validationLog.assertionError("Value does not match Date Stamp pattern on line {}, column name '{}': value '{}'", lineNumber, column.getName(), value);
 			} else {
 				BigInteger maxDate = dateStamp.getMaxDate();
 				if (maxDate != null) {
 					Integer valueInt = Integer.valueOf(value);
 					int maxDateInt = maxDate.intValue();
 					if (valueInt > maxDateInt) {
-						LOGGER.error("Value is a date after the maximum date on line {}, column name '{}': maximum date '', actual date '{}'", lineNumber, column.getName(), maxDateInt, value);
+						validationLog.assertionError("Value is a date after the maximum date on line {}, column name '{}': maximum date '', actual date '{}'", lineNumber, column.getName(), maxDateInt, value);
 					}
 				}
 			}
@@ -136,7 +142,7 @@ public class ColumnPatternTest {
 		// Component SCT ID
 		if (column.getSctid() != null) {
 			if (!COMPONENT_ID_PATTERN.matcher(value).matches()) {
-				LOGGER.error("Value does not match Component ID pattern on line {}, column name '{}': value '{}'", lineNumber, column.getName(), value);
+				validationLog.assertionError("Value does not match SCTID pattern on line {}, column name '{}': value '{}'", lineNumber, column.getName(), value);
 			}
 		}
 
@@ -144,7 +150,7 @@ public class ColumnPatternTest {
 		String regex = column.getRegex();
 		if (regex != null) {
 			if (regexCache.get(regex).matcher(value).matches()) {
-				LOGGER.error("Value does not match custom regex pattern on line {}, column name '{}': pattern '{}', value '{}'", lineNumber, column.getName(), regex, value);
+				validationLog.assertionError("Value does not match custom regex pattern on line {}, column name '{}': pattern '{}', value '{}'", lineNumber, column.getName(), regex, value);
 			}
 		}
 	}
@@ -152,7 +158,7 @@ public class ColumnPatternTest {
 	private void testHeaderValue(String value, ColumnPatternTestConfiguration.File.Column column) {
 		String expectedColumnName = column.getName();
 		if (!expectedColumnName.equals(value)) {
-			LOGGER.error("Column name does not match expected value: expected '{}', actual '{}'", expectedColumnName, value);
+			validationLog.assertionError("Column name does not match expected value: expected '{}', actual '{}'", expectedColumnName, value);
 			// TODO: Should we stop testing the file if we reach this point?
 		}
 	}
@@ -167,7 +173,7 @@ public class ColumnPatternTest {
 						Pattern compiledRegex = Pattern.compile(regex);
 						regexCache.put(regex, compiledRegex);
 					} catch (PatternSyntaxException e) {
-						LOGGER.error("Regex invalid for file {} column {}", file.getName(), column.getName(), e);
+						validationLog.configurationError("Regex invalid for file {} column {}", file.getName(), column.getName(), e);
 					}
 				}
 			}
