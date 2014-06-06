297b024afa43f22c49550beaebd71ccc694b5385
bronwen-cassidy <bronwen.cassidy@gmail.com>
2014-06-06 20:19:29 +0100

    Libraries, and spring config in place
    Modules in place, war file building, jar ready to run with tomcat.
    Transactions, annotations and initial dependencies done.

---
+++ api/src/main/java/org/ihtsdo/rvf/controller/AssertionController.java
@@ -0,0 +1,14 @@
+package org.ihtsdo.rvf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * User: Bronwen Cassidy
+ * Date: 06/06/2014
+ * Time: 10:57
+ */
+@Controller
+@RequestMapping("/assertions")
+public class AssertionController {
+}
---
+++ model/src/main/java/org/ihtsdo/rvf/entity/Assertion.java
@@ -0,0 +1,18 @@
+package org.ihtsdo.rvf.entity;
+
+/**
+ * User: Bronwen Cassidy
+ * Date: 06/06/2014
+ * Time: 13:40
+ */
+
+import javax.persistence.Entity;
+
+/**
+ * An Assertion represents a truth in snomed, it consists of a number of tests to verify
+ * the assertion.
+ */
+@Entity
+public class Assertion {
+
+}
