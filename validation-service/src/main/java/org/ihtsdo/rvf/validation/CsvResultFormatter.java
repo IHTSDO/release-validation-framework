777ab0727b5952463c4328f7d451f5ba5d3407a8
Kai Kewley <kaikewley@gmail.com>
2014-07-07 09:08:41 +0100

    Brought test result to first column in report with value pass/fail.

--- validation-service/src/main/java/org/ihtsdo/rvf/validation/CsvResultFormatter.java
+++ validation-service/src/main/java/org/ihtsdo/rvf/validation/CsvResultFormatter.java
@@ -14,12 +14,14 @@ public class CsvResultFormatter implements ResultFormatter {
         output.append(headers).append(String.format("%n"));
         for (TestRunItem ti : testRuns) {
             // todo color code
-            output.append(String.format("%s, %s, %s, %s, %s, %s, %s, %s, %d, %s%n", ti.getExecutionId(),
+            output.append(String.format("%s, %s, %s, %s, %s, %s, %s, %s, %d, %s%n",
+					ti.isFailure() ? "fail" : "pass",
+					ti.getExecutionId(),
                     ti.getStartDate(), ti.getFileName(), ti.getFilePath(), ti.getColumnName(), ti.getTestType(),
-                    ti.getTestPattern(), ti.isFailure(), failures.size(), "need to get failure examples"));
+                    ti.getTestPattern(), failures.size(), "need to get failure examples"));
         }
         return output.toString();
     }
 
-    private static final String headers = "lineNumber-columnNumber, testExecutionStartDate, fileName, filePath, columnName, testType, testPattern, testResult, failureCount, failed Value";
+    private static final String headers = "testResult, lineNumber-columnNumber, testExecutionStartDate, fileName, filePath, columnName, testType, testPattern, failureCount, failed Value";
 }
