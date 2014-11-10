8425dd87eb29c9e1747b8b25a7ace891c8992d26
Jay Kola <jay@noesisinformatica.com>
2014-11-10 14:05:57 +0000

    RVF-96 INPROGRESS: Renamed UploadFileControllerTest to IntegrationTests so they are skipped on Jenkins - this test also loads the spring application context which then tries to access to MySQL database

--- api/src/test/java/org/ihtsdo/rvf/controller/TestUploadFileControllerIntegrationTest.java
+++ api/src/test/java/org/ihtsdo/rvf/controller/TestUploadFileControllerIntegrationTest.java
@@ -23,7 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"/testDispatcherServletContext.xml"})
 @WebAppConfiguration
-public class TestUploadFileControllerTest {
+public class TestUploadFileControllerIntegrationTest {
 
     protected MockMvc mockMvc;
 
