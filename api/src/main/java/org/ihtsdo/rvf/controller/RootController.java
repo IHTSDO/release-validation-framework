d6ca06e84ef1de50203fc4a61c59affeae261e52
Jay Kola <jay@noesisinformatica.com>
2014-12-19 10:01:05 +0000

    RVF-108: INPROGRESS : Adding custom servlet and hypermedia support based on SRS conventions

--- api/src/main/java/org/ihtsdo/rvf/controller/RootController.java
+++ api/src/main/java/org/ihtsdo/rvf/controller/RootController.java
@@ -1,50 +1,33 @@
 package org.ihtsdo.rvf.controller;
 
-import org.ihtsdo.rvf.entity.AssertionTest;
-import org.ihtsdo.rvf.helper.JsonEntityGenerator;
-import org.ihtsdo.rvf.service.AssertionService;
+import org.ihtsdo.rvf.helper.HypermediaGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
- * temporary it will eventually map to /index and will load a view of the specific release centers
- * assertion test combinations. Don't want it loaded yet as i want to be able to create the assertions
- * and create the tests then we can discuss the business behind shared assertions.
- * Each release center has it's own combinations of assertionTests
+ * Root controller used to generate HATEOAS style hypermedia links
  */
 
 @Controller
 @RequestMapping("/")
 public class RootController {
 
-	@Autowired
-	private AssertionService assertionService;
+    @Autowired
+    private HypermediaGenerator hypermediaGenerator;
 
-	@Autowired
-	private JsonEntityGenerator entityGenerator;
+    private static final String[] ROOT_LINK = {"assertions", "tests", "groups", "releases", "version"};
 
-	@RequestMapping
-	@ResponseBody
-	public List<Map<String, Object>> getAssertions(HttpServletRequest request) {
-		// todo pass through the release center should be part of security
-		List<AssertionTest> assertions = new ArrayList<>();//assertionService.findAll();
-
-		return entityGenerator.getEntityCollection(assertions, request);
-	}
-
-	@RequestMapping("/{id}")
-	@ResponseBody
-	public Map<String, Object> getBuild(@PathVariable Long id) {
-		AssertionTest assertionTest = new AssertionTest(id, "some label"); //assertionService.find(id);
-		return entityGenerator.getEntity(assertionTest);
-	}
+    @RequestMapping
+    @ResponseBody
+    public Map<String, Object> getRoot(HttpServletRequest request) {
+        boolean currentResource = true;
+        return hypermediaGenerator.getEntityHypermedia(new HashMap<String, String>(), currentResource, request, ROOT_LINK);
+    }
 
 }
---
+++ api/src/main/java/org/ihtsdo/rvf/helper/HypermediaGenerator.java
@@ -0,0 +1,105 @@
+package org.ihtsdo.rvf.helper;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Utility class for generating Hypermedia based links - taken from SRS build cloud
+ */
+@Component
+public class HypermediaGenerator {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    public List<Map<String, Object>> getEntityCollectionHypermedia(Collection<?> entities, HttpServletRequest request, String[] entityLinks) {
+        return getEntityCollectionHypermedia(entities, request, entityLinks, null);
+    }
+
+    public List<Map<String,Object>> getEntityCollectionHypermedia(Collection<?> entities, HttpServletRequest request) {
+        return getEntityCollectionHypermedia(entities, request, null, null);
+    }
+
+    public List<Map<String, Object>> getEntityCollectionHypermedia(Collection<?> entities, HttpServletRequest request, String[] entityLinks, String instanceRoot) {
+        String url = getUrl(request);
+        String apiRootUrl = getApiRootUrl(url, request);
+        if (instanceRoot != null) {
+            url = apiRootUrl + instanceRoot;
+        }
+        List<Map<String, Object>> entitiesHypermedia = new ArrayList<>();
+        for (Object entity : entities) {
+            entitiesHypermedia.add(getEntityHypermedia(entity, false, url, apiRootUrl, entityLinks));
+        }
+        return entitiesHypermedia;
+    }
+
+    /**
+     *
+     * @param entity
+     * @param currentResource - if true indicates that the URL already contains the entity id.  If false, the entity will be added.
+     * @param request
+     * @param entityLinks
+     * @return
+     */
+    public Map<String, Object> getEntityHypermedia(Object entity, boolean currentResource, HttpServletRequest request, String... entityLinks) {
+        String url = getUrl(request);
+        String apiRootUrl = getApiRootUrl(url, request);
+        return getEntityHypermedia(entity, currentResource, url, apiRootUrl, entityLinks);
+    }
+
+    public Map<String, Object> getEntityHypermediaOfAction(Object entity, HttpServletRequest request, String... entityLinks) {
+        String url = getUrl(request);
+        // Remove action name
+        url = url.substring(0, url.lastIndexOf("/"));
+        String apiRootUrl = getApiRootUrl(url, request);
+        return getEntityHypermedia(entity, true, url, apiRootUrl, entityLinks);
+    }
+
+    private Map<String, Object> getEntityHypermedia(Object entity, boolean currentResource, String url, String apiRootUrl, String... entityLinks) {
+        @SuppressWarnings("unchecked")
+        Map<String,Object> entityMap = objectMapper.convertValue(entity, Map.class);
+        if (!currentResource) {
+            url = url + "/" + entityMap.get("id");
+        }
+        if (entityMap != null) {
+            entityMap.put("url", url);
+            if (entityLinks != null) {
+                for (String link : entityLinks) {
+                    String linkName;
+                    if (link.contains("|")) {
+                        String[] linkParts = link.split("\\|");
+                        linkName = linkParts[0];
+                        link = linkParts[1];
+                    } else {
+                        linkName = link.replace("/", "");
+                    }
+                    String linkUrl = (link.startsWith("/") ? apiRootUrl : (url + "/")) + link;
+                    entityMap.put(linkName + "_url", linkUrl);
+                }
+            }
+        }
+        return entityMap;
+    }
+
+    private String getUrl(HttpServletRequest request) {
+        String requestUrl = request.getRequestURL().toString();
+        // Remove any trailing slash
+        if (requestUrl.lastIndexOf('/') == requestUrl.length() - 1) {
+            requestUrl = requestUrl.substring(0, requestUrl.length() - 1);
+        }
+        return requestUrl;
+    }
+
+    private String getApiRootUrl(String url, HttpServletRequest request) {
+        String rootPath = request.getContextPath() + request.getServletPath();
+        return url.substring(0, url.indexOf(rootPath) + rootPath.length());
+    }
+
+}
