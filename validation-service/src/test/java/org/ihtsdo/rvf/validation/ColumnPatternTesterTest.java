6eaa2e678b108a49a268451bf7041245dd4bce42
CoderMChu <mch@ihtsdo.org>
2015-01-22 08:45:01 +0000

    Update test for changes made in [RVF-131]

--- validation-service/src/test/java/org/ihtsdo/rvf/validation/ColumnPatternTesterTest.java
+++ validation-service/src/test/java/org/ihtsdo/rvf/validation/ColumnPatternTesterTest.java
@@ -1,18 +1,23 @@
 package org.ihtsdo.rvf.validation;
 
-import org.ihtsdo.rvf.validation.impl.CsvResultFormatter;
-import org.ihtsdo.rvf.validation.impl.StreamTestReport;
-import org.ihtsdo.rvf.validation.log.impl.TestValidationLogImpl;
-import org.ihtsdo.rvf.validation.resource.ResourceManager;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
 import java.net.URISyntaxException;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import org.ihtsdo.rvf.validation.impl.CsvResultFormatter;
+import org.ihtsdo.rvf.validation.impl.StreamTestReport;
+import org.ihtsdo.rvf.validation.log.impl.TestValidationLogImpl;
+import org.ihtsdo.rvf.validation.resource.ResourceManager;
+import org.junit.Test;
 
 public class ColumnPatternTesterTest {
 
@@ -21,7 +26,7 @@ public class ColumnPatternTesterTest {
 
 	@Test
 	public void testFileNotFound() throws Exception {
-		ResourceManager resourceManager = new TestFileResourceProvider(new File(""));
+		final ResourceManager resourceManager = new TestFileResourceProvider(new File(""));
 		testReport = new StreamTestReport(new CsvResultFormatter(), new TestWriterDelegate(new StringWriter()), false);
 		tester = new ColumnPatternTester(new TestValidationLogImpl(ColumnPatternTester.class), resourceManager, testReport);
 
@@ -33,16 +38,16 @@ public class ColumnPatternTesterTest {
 
 	@Test
 	public void testInvalidId() throws Exception {
-		String filename = "/der2_sRefset_SimpleMapDelta_INT_20140131.txt";
+		final String filename = "/der2_sRefset_SimpleMapDelta_INT_20140131.txt";
 		executeRun(filename, false);
 
 		assertEquals("the 2 invalid ids", 2, testReport.getNumErrors());
-		assertEquals(228, testReport.getNumSuccesses());
+		assertEquals(229, testReport.getNumSuccesses());
 	}
 
 	@Test
 	public void testAssociationRefSet() throws Exception {
-		String filename = "/der2_cRefset_AssociationReferenceDelta_20140731.txt";
+		final String filename = "/der2_cRefset_AssociationReferenceDelta_20140731.txt";
 		executeRun(filename, false);
 
 		assertEquals("Not an RF2 specification file", 1, testReport.getNumErrors());
@@ -51,7 +56,7 @@ public class ColumnPatternTesterTest {
 
 	@Test
 	public void testOnlyHeaders() throws Exception {
-		String filename = "/der2_Refset_SimpleDelta_INT_20140131.txt";
+		final String filename = "/der2_Refset_SimpleDelta_INT_20140131.txt";
 		executeRun(filename, false);
 
 		assertEquals(0, testReport.getNumErrors());
@@ -60,7 +65,7 @@ public class ColumnPatternTesterTest {
 
 	@Test
 	public void testEmptyEffectiveTime() throws Exception {
-		String filename = "/sct2_TextDefinition_Delta-en_INT_20140731.txt";
+		final String filename = "/sct2_TextDefinition_Delta-en_INT_20140731.txt";
 		executeRun(filename, false);
 
 		assertEquals("Invalid Effective Time there are 9 rows hence 9 errors", 9, testReport.getNumErrors());
@@ -68,7 +73,7 @@ public class ColumnPatternTesterTest {
 
 	@Test
 	public void testComplexMapEmptyId() throws Exception {
-		String filename = "/der2_iissscRefset_ComplexMapDelta_INT_20140731.txt";
+		final String filename = "/der2_iissscRefset_ComplexMapDelta_INT_20140731.txt";
 		executeRun(filename, false);
 
 		assertEquals("No id by 9 rows", 9, testReport.getNumErrors());
@@ -76,7 +81,7 @@ public class ColumnPatternTesterTest {
 
 	@Test
 	public void testExtraColumn() throws Exception {
-		String filename = "/der2_ssRefset_ModuleDependencyDelta_INT_20140731.txt";
+		final String filename = "/der2_ssRefset_ModuleDependencyDelta_INT_20140731.txt";
 		executeRun(filename, false);
 
 		assertEquals("Only 4 as we abort the rest", 4, testReport.getNumErrors());
@@ -84,7 +89,7 @@ public class ColumnPatternTesterTest {
 
 	@Test
 	public void testExtraColumnAtEnd() throws Exception {
-		String filename = "/der2_ssRefset_ModuleDependencyDelta_INT_20140831.txt";
+		final String filename = "/der2_ssRefset_ModuleDependencyDelta_INT_20140831.txt";
 		executeRun(filename, false);
 
 		assertEquals("only the 4 errors i column 3 rows", 4, testReport.getNumErrors());
@@ -92,7 +97,7 @@ public class ColumnPatternTesterTest {
 
 	@Test
 	public void testInvalidColumnName() throws Exception {
-		String filename = "/sct2_Relationship_Full_INT_20140731.txt";
+		final String filename = "/sct2_Relationship_Full_INT_20140731.txt";
 		executeRun(filename, false);
 
 		assertEquals("Just the header", 1, testReport.getNumErrors());
@@ -100,7 +105,7 @@ public class ColumnPatternTesterTest {
 
 	@Test
 	public void testInvalidModuleId() throws Exception {
-		String filename = "/sct2_StatedRelationship_Snapshot_INT_20140731.txt";
+		final String filename = "/sct2_StatedRelationship_Snapshot_INT_20140731.txt";
 		executeRun(filename, false);
 		assertEquals("9 rows contain this invalid module id, so one error reported with a count of 9 row 1 being the first occurence", 9, testReport.getNumErrors());
 		assertEquals("how many errors were actually written out", 1, testReport.getNumberRecordedErrors());
@@ -108,7 +113,7 @@ public class ColumnPatternTesterTest {
 
 	@Test
 	public void testMissingId() throws Exception {
-		String filename = "/der2_cRefset_AssociationReferenceDelta_INT_20140731.txt";
+		final String filename = "/der2_cRefset_AssociationReferenceDelta_INT_20140731.txt";
 		executeRun(filename, false);
 
 		assertEquals("1 header + 9 rows contain missing id", 10, testReport.getNumErrors());
@@ -116,7 +121,7 @@ public class ColumnPatternTesterTest {
 
 	@Test
 	public void testMissingModuleId() throws Exception {
-		String filename = "/der2_cRefset_LanguageDelta-en_INT_20140731.txt";
+		final String filename = "/der2_cRefset_LanguageDelta-en_INT_20140731.txt";
 		executeRun(filename, false);
 
 		assertEquals("1 header + 9 rows contain missing id", 10, testReport.getNumErrors());
@@ -124,7 +129,7 @@ public class ColumnPatternTesterTest {
 
 	@Test
 	public void testMixedErrors() throws Exception {
-		String filename = "/der2_cRefset_LanguageSnapshot-en_INT_20140731.txt";
+		final String filename = "/der2_cRefset_LanguageSnapshot-en_INT_20140731.txt";
 		executeRun(filename, false);
 
 		assertEquals("1 header + 9 rows contain missing id", 32, testReport.getNumErrors());
@@ -132,7 +137,7 @@ public class ColumnPatternTesterTest {
 
 	@Test
 	public void testSpacesAtEnd() throws Exception {
-		String filename = "/rel2_Refset_SimpleDelta_INT_20140428.txt";
+		final String filename = "/rel2_Refset_SimpleDelta_INT_20140428.txt";
 		executeRun(filename, false);
 
 		assertEquals("1 row contains a tab a the end + 1 row contains 2 spaces at end + the last column of the row with spaces will fail column pattern test ", 3, testReport.getNumErrors());
@@ -140,7 +145,7 @@ public class ColumnPatternTesterTest {
 
 	@Test
 	public void testTabsInBetween() throws Exception {
-		String filename = "/rel2_Refset_SimpleDelta_INT_20140428.txt";
+		final String filename = "/rel2_Refset_SimpleDelta_INT_20140428.txt";
 		executeRun(filename, false);
 
 		assertEquals("1 row contains a tab a the end + 1 row contains 2 spaces at end + the last column of the row with spaces will fail column pattern test ", 3, testReport.getNumErrors());
@@ -148,16 +153,16 @@ public class ColumnPatternTesterTest {
 
 	@Test
 	public void testBlankRow() throws Exception {
-		String filename = "/rel2_Refset_SimpleDelta_INT_20130422.txt";
+		final String filename = "/rel2_Refset_SimpleDelta_INT_20130422.txt";
 		executeRun(filename, false);
 
 		assertEquals("1 blank row ", 1, testReport.getNumErrors());
 	}
 
-	public void executeRun(String filename, boolean writeSucceses) throws URISyntaxException {
-		File f = new File(getClass().getResource(filename).toURI());
+	public void executeRun(final String filename, final boolean writeSucceses) throws URISyntaxException {
+		final File f = new File(getClass().getResource(filename).toURI());
 
-		ResourceManager resourceManager = new TestFileResourceProvider(f);
+		final ResourceManager resourceManager = new TestFileResourceProvider(f);
 		//testReport = new TestReport(new CsvResultFormatter());
 
 		testReport = new StreamTestReport(new CsvResultFormatter(), new TestWriterDelegate(new StringWriter()), writeSucceses);
@@ -168,16 +173,16 @@ public class ColumnPatternTesterTest {
 
 	class TestFileResourceProvider implements ResourceManager {
 
-		private List<String> fileNames = new ArrayList<>();
-		private File file;
+		private final List<String> fileNames = new ArrayList<>();
+		private final File file;
 
-		public TestFileResourceProvider(File file) {
+		public TestFileResourceProvider(final File file) {
 			this.file = file;
 			fileNames.add(file.getName());
 		}
 
 		@Override
-		public BufferedReader getReader(String name, Charset charset) throws IOException {
+		public BufferedReader getReader(final String name, final Charset charset) throws IOException {
 			return new BufferedReader(new InputStreamReader(new FileInputStream(file)));
 		}
 
@@ -192,7 +197,7 @@ public class ColumnPatternTesterTest {
 		}
 
 		@Override
-		public boolean match(String name) {
+		public boolean match(final String name) {
 			return false;
 		}
 	}
