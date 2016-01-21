7baa9dcf3d6c1243244bd483bb1d2c42d367047d
CoderMChu <mch@ihtsdo.org>
2016-01-21 20:16:26 +0000

    Add delta export data loader

---
+++ execution-service/src/test/java/org/ihtsdo/rvf/execution/service/impl/ProspectiveReleaseDataLoaderTest.java
@@ -0,0 +1,49 @@
+package org.ihtsdo.rvf.execution.service.impl;
+
+import java.io.File;
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+import org.ihtsdo.otf.rest.exception.BusinessServiceException;
+import org.ihtsdo.rvf.execution.service.ReleaseDataManager;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/testExecutionServiceContext.xml"})
+public class ProspectiveReleaseDataLoaderTest {
+	@Resource(name = "snomedDataSource")
+	private DataSource snomedDataSource;
+	@Autowired
+	private ReleaseDataManager releaseDataManager;
+	
+	ProspectiveReleaseDataLoader dataLoader;
+	private String prospectiveVersion;
+	
+	@Before
+	public void setUp() {
+		ValidationRunConfig validationConfig = new ValidationRunConfig();
+		validationConfig.setRf2DeltaOnly(true);
+		validationConfig.setFailureExportMax(100);
+		validationConfig.setFirstTimeRelease(false);
+		validationConfig.setPrevIntReleaseVersion("int_20160131");
+		File localProspectiveFile = new File(ClassLoader.getSystemResource("Daily_Export_Delta.zip").getFile());
+		validationConfig.setLocalProspectiveFile(localProspectiveFile);
+		dataLoader = new ProspectiveReleaseDataLoader(validationConfig, releaseDataManager);
+		prospectiveVersion = Long.toString(System.currentTimeMillis());
+	}
+	
+	@Test
+	public void testConstructProspectiveVersionWithRF2DeltaOnly() throws BusinessServiceException {
+		List<String> filesLoaded = dataLoader.loadProspectiveDeltaWithPreviousSnapshotIntoDB(prospectiveVersion);
+		Assert.assertEquals(1, filesLoaded.size());
+		Assert.assertTrue(releaseDataManager.isKnownRelease(prospectiveVersion));
+	}
+}
