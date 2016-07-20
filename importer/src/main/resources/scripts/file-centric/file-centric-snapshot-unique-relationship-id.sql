88c76be9b2f705333fb70ebebbcced28d7b289bd
CoderMChu <mch@ihtsdo.org>
2016-07-20 15:38:02 +0100

    Update relationship ids assertion by removing active flag

--- importer/src/main/resources/scripts/file-centric/file-centric-snapshot-unique-relationship-id.sql
+++ importer/src/main/resources/scripts/file-centric/file-centric-snapshot-unique-relationship-id.sql
@@ -3,7 +3,7 @@
 	file-centric-snapshot-unique-relationship-id.sql
 
 	Assertion:
-	Active stated and inferred relationship ids should be unique.
+	Stated and inferred relationship ids should be unique.
 
 ********************************************************************************/
 	insert into qa_result (runid, assertionuuid, concept_id, details)
@@ -13,6 +13,4 @@
 		a.sourceid,
 		concat('Relationship: id=',a.id, ' is duplicated in the stated and inferred relationships') 	
 	from curr_stated_relationship_s a, curr_relationship_s b
-	where a.id=b.id
-	and a.active=1
-	and b.active=1;
+	where a.id=b.id;
