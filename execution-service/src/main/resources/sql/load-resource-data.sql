17e23447b562852b2b25b55ca11b0d26e25547db
CoderMChu <mch@ihtsdo.org>
2017-04-19 11:08:32 +0100

    RVF-271 and APDS-141 changes

--- execution-service/src/main/resources/sql/load-resource-data.sql
+++ execution-service/src/main/resources/sql/load-resource-data.sql
@@ -2,26 +2,26 @@ load data local
 	infile '<data_location>/cs_words.txt'
 	into table res_casesensitiveTerm
 	columns terminated by '\t' 
-	lines terminated by '\r\n' 
+	lines terminated by '\n' 
 	ignore 1 lines;
 	
 load data local 
 	infile '<data_location>/usTerms.txt'
 	into table res_usterm
 	columns terminated by '\t' 
-	lines terminated by '\r\n' 
+	lines terminated by '\n' 
 	ignore 1 lines;
 
 load data local 
 	infile '<data_location>/gbTerms.txt'
 	into table res_gbterm
 	columns terminated by '\t' 
-	lines terminated by '\r\n' 
+	lines terminated by '\n' 
 	ignore 1 lines;
 
 load data local 
 	infile '<data_location>/semanticTags.txt'
 	into table res_semantictag
 	columns terminated by '\t' 
-	lines terminated by '\r\n'
+	lines terminated by '\n'
 	ignore 1 lines;
