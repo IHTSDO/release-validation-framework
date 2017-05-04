a65d5b0ac8a00ebc71929594f1633e58b926775f
CoderMChu <mch@ihtsdo.org>
2017-05-04 20:40:38 +0100

    Fix MySQL server version for the right syntax to use near 'ENGINE = MyISAM' issue

--- importer/src/main/resources/scripts/component-centric/component-centric-delta-language-lang-specific-words-us-call-proc.sql
+++ importer/src/main/resources/scripts/component-centric/component-centric-delta-language-lang-specific-words-us-call-proc.sql
@@ -9,12 +9,7 @@
 ********************************************************************************/
 
 	drop table if exists v_curr_delta_us;
-	create table if not exists v_curr_delta_us
-	(	id varchar(36),
-		term varchar(255),
-		conceptid varchar(255)
-	) ENGINE=MyISAM;
-	
+	create table v_curr_delta_us (id varchar(36),term varchar(255),conceptid varchar(255)) default charset=utf8;
 	insert into v_curr_delta_us
 	select distinct a.id, a.term,a.conceptid
 		from curr_description_d a 
