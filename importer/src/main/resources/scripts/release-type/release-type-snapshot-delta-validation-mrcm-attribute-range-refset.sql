0277951627a703f7a3b02fa92262ea45223f39b2
HMVNLTP046\QuyenLy <Quyen.Ly@Huntermacdonald.com>
2017-05-18 12:23:26 +0700

    Correct table for MRCM Reelase Type validation : The current data in the MRCM ATTRIBUTE RANGE REFSET snapshot file are the same as the data in the current delta file

--- importer/src/main/resources/scripts/release-type/release-type-snapshot-delta-validation-mrcm-attribute-range-refset.sql
+++ importer/src/main/resources/scripts/release-type/release-type-snapshot-delta-validation-mrcm-attribute-range-refset.sql
@@ -12,18 +12,16 @@
 		'<ASSERTIONUUID>',
 		a.referencedcomponentid,
 		concat('MRCM ATTRIBUTE RANGE REFSET: id=',a.id, ' is in delta file but not in snapshot file.') 	
-	from curr_mrcmattributedomainrefset_d a
-	left join curr_mrcmattributedomainrefset_s b
+	from curr_mrcmAttributeRangeRefset_d a
+	left join curr_mrcmAttributeRangeRefset_s b
 		on a.id = b.id
 		and a.effectivetime = b.effectivetime
 		and a.active = b.active
 		and a.moduleid = b.moduleid
 		and a.refsetid = b.refsetid
-		and a.referencedcomponentid = b.referencedcomponentid		
-		and a.domainid = b.domainid
-		and a.grouped = b.grouped
-		and a.attributecardinality = b.attributecardinality
-		and a.attributeingroupcardinality = b.attributeingroupcardinality		
+		and a.referencedcomponentid = b.referencedcomponentid
+		and a.rangeconstraint = b.rangeconstraint
+		and a.attributerule = b.attributerule
 		and a.rulestrengthid = b.rulestrengthid
 		and a.contenttypeid = b.contenttypeid
 	where b.id is null
@@ -32,9 +30,7 @@
 	or b.moduleid is null
 	or b.refsetid is null
 	or b.referencedcomponentid is null	
-	or b.domainid is null
-	or b.grouped is null
-	or b.attributecardinality is null
-	or b.attributeingroupcardinality is null
+	or b.rangeconstraint is null
+	or b.attributerule is null
 	or b.rulestrengthid is null
 	or b.contenttypeid is null;
