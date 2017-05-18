fee45608604bd070311b33b7a045aed0eeb72782
Nam Le <nam.le@huntermacdonald.com>
2017-05-18 17:02:00 +0700

    [RQI-33] Update release-type-validation for item: "•The current <MRCM refset  file name> snapshot file is an accurate derivative of the current full file".

---
+++ importer/src/main/resources/scripts/release-type/release-type-snapshot-validation-mrcm-attribute-domain-refset.sql
@@ -0,0 +1,85 @@
+/*  
+	The current MRCM Attribute Domain Refset snapshot file is an accurate derivative of the current full file
+*/
+
+/* 	view of current snapshot, derived from current full */
+	drop table if exists temp_mrcmattributedomainrefset_v;
+  	create table if not exists temp_mrcmattributedomainrefset_v like curr_mrcmattributedomainrefset_f;
+  	insert into temp_mrcmattributedomainrefset_v
+	select a.*
+	from curr_mrcmattributedomainrefset_f a
+	where cast(a.effectivetime as datetime) =
+		(select max(cast(z.effectivetime as datetime))
+		 from curr_mrcmattributedomainrefset_f z
+		 where z.id = a.id);
+
+/* in the snapshot; not in the full */
+	insert into qa_result (runid, assertionuuid, concept_id, details)
+	select
+		<RUNID>,
+		'<ASSERTIONUUID>',
+		a.sourceid,
+		concat('MRCM Attribute Domain Refset: id=',a.id, ' is in SNAPSHOT file, but not in FULL file.')
+	from curr_mrcmattributedomainrefset_s a
+	left join temp_mrcmattributedomainrefset_v b
+		on a.id = b.id
+		and a.effectivetime = b.effectivetime
+		and a.active = b.active
+		and a.moduleid = b.moduleid
+		and a.refsetid = b.refsetid
+		and a.referencedcomponentid = b.referencedcomponentid
+		and a.domainid = b.domainid
+		and a.grouped = b.grouped
+		and a.attributecardinality = b.attributecardinality
+		and a.attributeingroupcardinality = b.attributeingroupcardinality
+		and a.rulestrengthid = b.rulestrengthid
+		and a.contenttypeid = b.contenttypeid
+	where b.id is null
+        or b.effectivetime is null
+        or b.active is null
+        or b.moduleid is null
+        or b.refsetid is null
+        or b.referencedcomponentid is null
+        or b.domainid is null
+        or b.grouped is null
+        or b.attributecardinality is null
+        or b.attributeingroupcardinality is null
+        or b.rulestrengthid is null
+        or b.contenttypeid is null;
+
+/* in the full; not in the snapshot */
+	insert into qa_result (runid, assertionuuid, concept_id, details)
+	select
+		<RUNID>,
+		'<ASSERTIONUUID>',
+		a.sourceid,
+		concat('MRCM Attribute Domain Refset: id=',a.id, ' is in FULL file, but not in SNAPSHOT file.')
+	from temp_mrcmattributedomainrefset_v a
+	left join curr_mrcmattributedomainrefset_s b
+		on a.id = b.id
+		and a.effectivetime = b.effectivetime
+		and a.active = b.active
+		and a.moduleid = b.moduleid
+		and a.refsetid = b.refsetid
+		and a.referencedcomponentid = b.referencedcomponentid
+		and a.domainid = b.domainid
+		and a.grouped = b.grouped
+		and a.attributecardinality = b.attributecardinality
+		and a.attributeingroupcardinality = b.attributeingroupcardinality
+		and a.rulestrengthid = b.rulestrengthid
+		and a.contenttypeid = b.contenttypeid
+	where b.id is null
+        or b.effectivetime is null
+        or b.active is null
+        or b.moduleid is null
+        or b.refsetid is null
+        or b.referencedcomponentid is null
+        or b.domainid is null
+        or b.grouped is null
+        or b.attributecardinality is null
+        or b.attributeingroupcardinality is null
+        or b.rulestrengthid is null
+        or b.contenttypeid is null;
+
+commit;
+drop table if exists temp_mrcmattributedomainrefset_v;
