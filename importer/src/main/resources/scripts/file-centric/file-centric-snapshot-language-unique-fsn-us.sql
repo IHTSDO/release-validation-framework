54b951eb9a3e47f313b9d6b0905177215bd17478
Nam Le <nam.le@huntermacdonald.com>
2017-05-23 11:19:11 +0700

    [RQI-33] Recover applicationContext files.

--- importer/src/main/resources/scripts/file-centric/file-centric-snapshot-language-unique-fsn-us.sql
+++ importer/src/main/resources/scripts/file-centric/file-centric-snapshot-language-unique-fsn-us.sql
@@ -3,10 +3,14 @@
 	file-centric-snapshot-language-unique-fsn-us
 
 	Assertion:
-	Every active FSN in the US module is referenced in the US lang refset
+	Every active concept defined in the US extension has a preferred FSN in the US language refset.
+	Every active FSN in the US module is referenced in the us-en lang reference set.
+
 
 ********************************************************************************/
-	
+	/*
+	Every active concept defined in the US extension has a preferred FSN in the US language refset.
+	*/
 	insert into qa_result (runid, assertionuuid, concept_id, details)
 	select 
 		<RUNID>,
@@ -26,3 +30,20 @@
 		and c.acceptabilityid='900000000000548007'
 		and b.conceptid= a.id);
 
+    /*
+    Every active FSN in the US module is referenced in the us-en lang reference set.
+    */
+    insert into qa_result (runid, assertionuuid, concept_id, details)
+	select
+		<RUNID>,
+		'<ASSERTIONUUID>',
+		a.id,
+		concat('FSN: id=',a.id, ' is not referenced in the US language refset.')
+    from curr_description_s a
+    where a.active = '1' and a.moduleid = '731000124108' and a.typeid = '900000000000003001' /*FSN*/
+    where not exists (select b.id
+    from curr_langrefset_s b
+    where b.referencedcomponentid = a.id
+    and b.active = '1'
+    and b.refsetid = '900000000000509007' /*US lang refset*/
+    and b.acceptabilityid='900000000000548007');
