a46971359544f7e210406f2427733c49590084a0
Huy Le <huy.le@huntermacdonald.com>
2017-12-01 13:10:04 +0700

    Fixed assertion for RVF-268

--- importer/src/main/resources/scripts/file-centric/file-centric-snapshot-description-term-duplicated.sql
+++ importer/src/main/resources/scripts/file-centric/file-centric-snapshot-description-term-duplicated.sql
@@ -13,7 +13,7 @@
 		a.conceptid,
 		concat('DESCRIPTION : ids =', GROUP_CONCAT(DISTINCT a.id ORDER BY a.id ASC SEPARATOR ', '), ' : Duplicated terms in DESCRIPTION snapshot.')
 	from curr_description_s a
-	where a.active = '1'
-	group by LOWER(a.term) 
+	where a.active = '1' and casesignificanceid = 900000000000448009
+	group by LOWER(a.term), a.conceptid, a.typeid
 	having COUNT(*) > 1;
 	commit;
