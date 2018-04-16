3cbce7a15de9ebb9f3696ac435b3f7f1174066dc
CoderMChu <mch@ihtsdo.org>
2018-04-16 17:47:11 +0100

    Commit changes for INFRA-2346 ATF-1530

--- importer/src/main/resources/scripts/component-centric/component-centric-snapshot-description-unique-term-in-concept.sql
+++ importer/src/main/resources/scripts/component-centric/component-centric-snapshot-description-unique-term-in-concept.sql
@@ -6,16 +6,34 @@
 
 	Implementation is limited to active descriptions of active concepts edited 
 	in the current prospective release.
+	Note: See another assertion component-centric-snapshot-description-unique-terms
 ********************************************************************************/
 /*  violators have the same term twice within a concept */
-	insert into qa_result (runid, assertionuuid, concept_id, details)
-	select 
-		<RUNID>,
-		'<ASSERTIONUUID>',
-		a.conceptid,
-		concat('DESC: Id=', a.id, ' contains non-unique term ', a.term) 
+
+drop table if exists tmp_description_edited;
+create table if not exists tmp_description_edited (
+	id bigint(20) not null,
+	languagecode varchar(2) not null,
+	conceptid bigint(20) not null,
+	term varchar(256) not null,
+	semantictag varchar(100));
+	
+insert into tmp_description_edited (id, languagecode, conceptid, term) 
+select a.id, a.languagecode, a.conceptid, a.term
 	from curr_description_s a
 	where a.active =1
-	and exists (select id from res_edited_active_concepts where id = a.conceptid)
-	group by a.conceptid, a.languagecode, binary a.term
-	having count(distinct a.id) > 1;
+	and exists (select id from res_edited_active_concepts where id = a.conceptid);
+	
+alter table tmp_description_edited add index idx_tmp_ds_cid (conceptId);
+alter table tmp_description_edited add index idx_tmp_ds_l (languagecode);
+alter table tmp_description_edited add index idx_tmp_ds_t (term);
+	
+insert into qa_result (runid, assertionuuid, concept_id, details)
+select 
+	<RUNID>,
+	'<ASSERTIONUUID>',
+	a.conceptid,
+	concat('Description: Id=', a.id, ' contains non-unique term ', a.term) 
+from tmp_description_edited a
+group by a.conceptid, a.languagecode, a.term
+having count(distinct a.id) > 1;
--- importer/src/main/resources/scripts/component-centric/component-centric-snapshot-description-unique-terms.sql
+++ importer/src/main/resources/scripts/component-centric/component-centric-snapshot-description-unique-terms.sql
@@ -6,34 +6,39 @@
 
 ********************************************************************************/
 
-drop table if exists temp_active_fsn_hierarchy;
+	drop table if exists temp_active_fsn_hierarchy;
 	create table if not exists temp_active_fsn_hierarchy as
 	select distinct a.conceptid, a.languagecode, concat('(',substring_index(a.term, '(', -1)) as semantictag
 	from curr_description_s a
-		join curr_description_d  b
-			on a.conceptid = b.conceptid
-			and a.active = 1
-			and a.typeid = '900000000000003001' /* fully specified name */
-		join curr_concept_s c
-			on c.id = b.conceptid
-			and c.active = 1;
-	commit;	 
-	
+	join curr_concept_s b
+		on a.conceptid = b.id
+	where a.active = 1
+		and b.active =1
+		and a.moduleid != '715515008'
+		and a.typeid = '900000000000003001'; /* fully specified name */
+		
 	alter table temp_active_fsn_hierarchy add index idx_tmp_afh_cid (conceptId);
 	alter table temp_active_fsn_hierarchy add index idx_tmp_afh_l (languagecode);
 	alter table temp_active_fsn_hierarchy add index idx_tmp_afh_st (semantictag);
 
 /* 	a list of descriptions and their hierarchies */
 	drop table if exists tmp_description_syn;
-	create table if not exists tmp_description_syn
+	create table if not exists tmp_description_syn (
+	id bigint(20) not null,
+	languagecode varchar(2) not null,
+	conceptid bigint(20) not null,
+	term varchar(256) not null,
+	semantictag varchar(100));
+	
+	insert into tmp_description_syn (id, languagecode, conceptid, term, semantictag) 
 	select a.id, a.languagecode, a.conceptid, a.term, b.semantictag as semantictag
 	from curr_description_s a
 	join temp_active_fsn_hierarchy b
-	on a.conceptid = b.conceptid
-	and a.active = 1
-	and a.languagecode = b.languagecode
-	where a.typeid = '900000000000013009'; /* syn */
-	commit;
+		on a.conceptid = b.conceptid
+		and a.languagecode = b.languagecode
+	where a.moduleid != '715515008'
+	and a.active =1
+	and a.typeid = '900000000000013009'; /* syn */
 	
 	alter table tmp_description_syn add index idx_tmp_ds_cid (conceptId);
 	alter table tmp_description_syn add index idx_tmp_ds_l (languagecode);
@@ -46,12 +51,10 @@ drop table if exists temp_active_fsn_hierarchy;
 		<RUNID>,
 		'<ASSERTIONUUID>',
 		a.conceptid,
-		concat(cast(a.term as binary), ' non-unique term within hierarchy ', a.semantictag)
+		concat('Synonym=', a.term, ' is duplicated within hierarchy ', a.semantictag)
 	from tmp_description_syn a,
 	(select a.term from tmp_description_syn a 
-		group by binary a.term, a.semantictag
+		group by a.term, a.semantictag
 		having count(a.id) > 1) as duplicate
 	where a.term = duplicate.term;
-	drop table if exists temp_active_fsn_hierarchy;
-	drop table if exists tmp_description_syn;
 	
