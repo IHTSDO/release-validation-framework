c10a88cde17ef44b906c07ca861e1fa981fb8508
CoderMChu <mch@ihtsdo.org>
2018-09-28 11:14:57 +0100

    MAINT-498 update assertion sql for performance fix

--- importer/src/main/resources/scripts/component-centric/component-centric-snapshot-description-unique-fsn-case-insensitive-checking.sql
+++ importer/src/main/resources/scripts/component-centric/component-centric-snapshot-description-unique-fsn-case-insensitive-checking.sql
@@ -5,23 +5,9 @@
 	Note: The failures of this assertion are reported as warning not error because there are cases that are not considered as duplicate.
 	(e.g Toxic effect of antimony and/or its compounds (disorder) is dupldate of Toxic effect of antimony AND/OR its compounds (disorder).
 	However Blood group antibody I (substance) and Blood group antibody i (substance) are not duplicate.)
-	The reason to create fsn_edited and active_fsns temp tables due to the collation of term in the description tables
+	The reason of using COLLATE utf8_general_ci is due to the collation of term in the description table
 	is in binary which is case sensitive but for checking FSN uniquenss it requires case insensitive. 
 ********************************************************************************/
-drop table if exists fsn_edited;
-create table fsn_edited (conceptid bigint(20), term varchar(255),key idx_fsn_edited(term)) default charset=utf8;
-
-insert into fsn_edited(conceptid,term) (select distinct b.conceptid,b.term from curr_concept_d a join curr_description_s b on a.id=b.conceptid
-where a.active=1 and b.active=1 and b.typeid ='900000000000003001');
-
-insert into fsn_edited(conceptid,term) (select distinct a.conceptid,a.term from curr_description_d a join curr_concept_s b on a.conceptid=b.id
-where a.active=1 and b.active=1 and a.typeid ='900000000000003001' and not exists (select conceptid from fsn_edited c where c.conceptid=a.conceptid));
-
-drop table if exists active_fsns;
-create table active_fsns (conceptid bigint(20),term varchar(255),key idx_active_fsn(term)) default charset=utf8;	
-	
-insert into active_fsns(conceptid,term) (select distinct b.conceptid,b.term from curr_concept_s a join curr_description_s b on a.id=b.conceptid 
-where a.active=1 and b.active=1 and b.typeid ='900000000000003001');
 
 insert into qa_result (runid, assertionuuid, concept_id, details)
 	select 
@@ -29,5 +15,7 @@ insert into qa_result (runid, assertionuuid, concept_id, details)
 	'<ASSERTIONUUID>',
 	a.conceptid,
 	concat('FSN=',a.term, ' concept=',a.conceptid, ': FSN term is not unique in description snapshot when case is ignored')
-	from fsn_edited a where exists (select count(*) total from active_fsns b where a.term = b.term having total >1)
-	and not exists ( select count(*) total from curr_description_s c where c.term = a.term having total >1);
+	from curr_description_d a,
+	(select b.term, count(b.id) as total from curr_description_s b join curr_concept_s c on b.conceptid=c.id
+	where b.active=1 and c.active=1 and b.typeid ='900000000000003001' group by term COLLATE utf8_general_ci having total > 1) result
+	where a.term COLLATE utf8_general_ci = result.term;
