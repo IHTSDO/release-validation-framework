be8cb3d09839b06ce907243e6974273986651218
CoderMChu <mch@ihtsdo.org>
2019-03-18 17:12:22 +0000

    MAINT-701: Add language code

--- importer/src/main/resources/scripts/component-centric/component-centric-snapshot-description-active-inactive-term-match-authoring.sql
+++ importer/src/main/resources/scripts/component-centric/component-centric-snapshot-description-active-inactive-term-match-authoring.sql
@@ -66,6 +66,7 @@
 	join tmp_inactive_desc b
 	on a.conceptid = b.conceptid
 	and a.moduleid = b.moduleid
+	and a.languagecode = b.languagecode
 	and cast(a.term as binary) = cast(b.term as binary)
 	where a.active != b.active
 	and cast(a.effectivetime as datetime) >= cast(b.effectivetime as datetime);
