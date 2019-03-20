b8dcf28461ab439f7a99d9866b3dd7c983cc9e80
Peter Groves Williams <petergwilliams@hotmail.com>
2019-03-20 15:54:09 +0000

    MAINT-708 Allow text definitions to have historical associations

--- importer/src/main/resources/scripts/file-centric/file-centric-snapshot-association-valid-referencedcomponentid.sql
+++ importer/src/main/resources/scripts/file-centric/file-centric-snapshot-association-valid-referencedcomponentid.sql
@@ -3,7 +3,7 @@
 	file-centric-snapshot-association-valid-referencedcomponentid
 
 	Assertion:
-	Referencedcomponentid refers to valid concepts in the ASSOCIATION REFSET snapshot file.
+	Referencedcomponentid refers to valid components in the ASSOCIATION REFSET snapshot file.
 
 ********************************************************************************/
 
@@ -16,6 +16,8 @@
 	from curr_associationrefset_s a
 	left join curr_concept_s b
 	on a.referencedcomponentid = b.id
-	where b.id is null and not exists ( select id from curr_description_s where id = a.referencedcomponentid );
+	where b.id is null 
+	and not exists ( select id from curr_description_s where id = a.referencedcomponentid )
+	and not exists ( select id from curr_textdefinition_s where id = a.referencedcomponentid );
 	commit;
 	
