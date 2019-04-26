adb2e298b9f305e1cc134fe148dea0458151c2a2
Huy Le <huyhu@netcompany.com>
2019-04-26 15:25:55 +0700

    MAINT-722: Fixed assertion "There is an equivalent definition in the en-us and en-gb language when provided"
    (cherry picked from commit 1e456564f8647200794f5887653cf7322264c278)

--- importer/src/main/resources/scripts/component-centric/component-centric-snapshot-definition-preferred-in-en_us-and-en_gb.sql
+++ importer/src/main/resources/scripts/component-centric/component-centric-snapshot-definition-preferred-in-en_us-and-en_gb.sql
@@ -10,8 +10,8 @@ insert into qa_result (runid, assertionuuid, concept_id, details)
 		'<ASSERTIONUUID>',
 		usTestDef.conceptid,
 		concat('ConceptId=', usTestDef.conceptid, ' has definition in EN-US but no equivalent found for EN-GB') 
-		from (select distinct a.conceptid from curr_textdefinition_d a left join curr_langrefset_s b on b.referencedcomponentid=a.id where b.refsetid=900000000000509007 and b.active=1 and a.active=1) usTestDef
-		left join (select distinct a.conceptid from curr_textdefinition_d a left join curr_langrefset_s b on b.referencedcomponentid=a.id where b.refsetid=900000000000508004 and b.active=1 and a.active=1) gbTextDef  
+		from (select distinct a.conceptid from curr_textdefinition_s a left join curr_langrefset_s b on b.referencedcomponentid=a.id where b.refsetid=900000000000509007 and b.active=1 and a.active=1) usTestDef
+		left join (select distinct a.conceptid from curr_textdefinition_s a left join curr_langrefset_s b on b.referencedcomponentid=a.id where b.refsetid=900000000000508004 and b.active=1 and a.active=1) gbTextDef
 		on usTestDef.conceptid = gbTextDef.conceptid 
 		where gbTextDef.conceptid is null;
 commit;
@@ -24,9 +24,9 @@ insert into qa_result (runid, assertionuuid, concept_id, details)
 		gbTextDef.conceptid,
 		concat('ConceptId=', gbTextDef.conceptid, ' has definition in EN-GB but no equivalent found for EN-US') 
 		from 
-		(select distinct a.conceptid from curr_textdefinition_d a left join curr_langrefset_s b on b.referencedcomponentid=a.id where b.refsetid=900000000000508004 and b.active=1 and a.active=1) gbTextDef 
+		(select distinct a.conceptid from curr_textdefinition_s a left join curr_langrefset_s b on b.referencedcomponentid=a.id where b.refsetid=900000000000508004 and b.active=1 and a.active=1) gbTextDef
 		left join 
-		(select distinct a.conceptid from curr_textdefinition_d a left join curr_langrefset_s b on b.referencedcomponentid=a.id where b.refsetid=900000000000509007 and b.active=1 and a.active=1) usTestDef
+		(select distinct a.conceptid from curr_textdefinition_s a left join curr_langrefset_s b on b.referencedcomponentid=a.id where b.refsetid=900000000000509007 and b.active=1 and a.active=1) usTestDef
 		on usTestDef.conceptid = gbTextDef.conceptid 
 		where usTestDef.conceptid is null;
 commit;
