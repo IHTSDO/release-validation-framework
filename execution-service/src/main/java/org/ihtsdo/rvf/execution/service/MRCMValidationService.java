00bd3dcd2f1442dd151075d6d30758f7b6f1c865
Huy Le <huyhu@netcompany.com>
2019-10-08 14:37:37 +0700

    RVF-342: Update MRCM validation on RVF to work with Extension products

--- execution-service/src/main/java/org/ihtsdo/rvf/execution/service/MRCMValidationService.java
+++ execution-service/src/main/java/org/ihtsdo/rvf/execution/service/MRCMValidationService.java
@@ -1,11 +1,16 @@
 package org.ihtsdo.rvf.execution.service;
 
+import com.google.common.collect.Sets;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.ihtsdo.otf.resourcemanager.ResourceManager;
 import org.ihtsdo.otf.rest.exception.BusinessServiceException;
 import org.ihtsdo.rvf.entity.FailureDetail;
 import org.ihtsdo.rvf.entity.TestRunItem;
 import org.ihtsdo.rvf.entity.TestType;
 import org.ihtsdo.rvf.entity.ValidationReport;
+import org.ihtsdo.rvf.execution.service.config.ValidationReleaseStorageConfig;
 import org.ihtsdo.rvf.execution.service.config.ValidationRunConfig;
 import org.ihtsdo.rvf.util.ZipFileUtils;
 import org.slf4j.Logger;
@@ -13,17 +18,40 @@ import org.slf4j.LoggerFactory;
 import org.snomed.quality.validator.mrcm.Assertion;
 import org.snomed.quality.validator.mrcm.ValidationRun;
 import org.snomed.quality.validator.mrcm.ValidationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class MRCMValidationService {
 
+	@Autowired
+	private ValidationReleaseStorageConfig releaseStorageConfig;
+
+	@Autowired
+	private ResourceLoader cloudResourceLoader;
+
+	private ResourceManager releaseSourceManager;
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(MRCMValidationService.class);
+
+	private static final String MRCM_PROSPECTIVE_FILE = "mrcm_prospective_file_";
+
+	private static final String MRCM_DEPENDENCY_FILE = "mrcm_dependency_file_";
+
+	@PostConstruct
+	public void init() {
+		releaseSourceManager = new ResourceManager(releaseStorageConfig, cloudResourceLoader);
+	}
 
 	public void runMRCMAssertionTests(final ValidationStatusReport statusReport, ValidationRunConfig validationConfig, String effectiveDate, Long executionId) {
 		File outputFolder = null;
@@ -31,7 +59,9 @@ public class MRCMValidationService {
 			final long timeStart = System.currentTimeMillis();
 			ValidationReport report = statusReport.getResultReport();
 			ValidationService validationService = new ValidationService();
-			ValidationRun validationRun = new ValidationRun(effectiveDate, true);
+			ValidationRun validationRun = new ValidationRun(
+					StringUtils.isNotBlank(effectiveDate) ? effectiveDate.replaceAll("-","") : effectiveDate
+					, false);
 			try {
 				outputFolder = extractZipFile(validationConfig, executionId);
 
@@ -107,17 +137,30 @@ public class MRCMValidationService {
 		return testRunItem;
 	}
 
-	private File extractZipFile(ValidationRunConfig validationConfig, Long executionId) throws BusinessServiceException {
+	private File extractZipFile(ValidationRunConfig validationConfig, Long executionId) throws BusinessServiceException, RVFExecutionException {
 		File outputFolder;
 		try{
-			outputFolder = new File(FileUtils.getTempDirectoryPath(), "rvf_loader_data_" + executionId);
-			LOGGER.info("MRCM output folder location = " + outputFolder.getAbsolutePath());
+			outputFolder = new File(FileUtils.getTempDirectoryPath(), MRCM_PROSPECTIVE_FILE + executionId);
+			LOGGER.info("Unzipped release folder location = " + outputFolder.getAbsolutePath());
 			if (outputFolder.exists()) {
-				LOGGER.info("MRCM output folder already exists and will be deleted before recreating.");
+				LOGGER.info("Unzipped release folder already exists and will be deleted before recreating.");
 				outputFolder.delete();
 			}
 			outputFolder.mkdir();
+
 			ZipFileUtils.extractFilesFromZipToOneFolder(validationConfig.getLocalProspectiveFile(), outputFolder.getAbsolutePath());
+			if (validationConfig.getExtensionDependency() != null && !validationConfig.isReleaseAsAnEdition()) {
+				if(StringUtils.isBlank(validationConfig.getExtensionDependency()) || !validationConfig.getExtensionDependency().endsWith(".zip")) {
+					throw new RVFExecutionException("MRCM validation cannot execute when Extension Dependency is empty or not a .zip file: " + validationConfig.getExtensionDependency());
+				}
+				InputStream dependencyStream = releaseSourceManager.readResourceStreamOrNullIfNotExists(validationConfig.getExtensionDependency());
+				File dependencyFile = File.createTempFile(MRCM_DEPENDENCY_FILE, ".zip");
+				OutputStream out = new FileOutputStream(dependencyFile);
+				IOUtils.copy(dependencyStream, out);
+				IOUtils.closeQuietly(dependencyStream);
+				IOUtils.closeQuietly(out);
+				ZipFileUtils.extractFilesFromZipToOneFolder(dependencyFile, outputFolder.getAbsolutePath());
+			}
 		} catch (final IOException ex){
 			final String errorMsg = String.format("Error while loading file %s.", validationConfig.getLocalProspectiveFile());
 			LOGGER.error(errorMsg, ex);
