c3436f4e704b3f073eaaba64e99dce5151fc48e1
CoderMChu <mch@ihtsdo.org>
2019-11-28 16:35:59 +0000

    MAINT-1090: Remove extra checking for inactivated owl expressions

--- importer/src/main/resources/scripts/release-type/release-type-snapshot-owl-expression-successive-states.sql
+++ importer/src/main/resources/scripts/release-type/release-type-snapshot-owl-expression-successive-states.sql
@@ -19,17 +19,4 @@ insert into qa_result (runid, assertionuuid, concept_id, details)
 	a.active = 0
 	and b.id is null;
 	commit;
-
-insert into qa_result (runid, assertionuuid, concept_id, details)
-	select
-		<RUNID>,
-		'<ASSERTIONUUID>',
-		a.referencedcomponentid,
-		concat('OWL Expression Refset: id=',a.id, '  is inactive but the OWL Expression was changed from previous release')
-	from curr_owlexpressionrefset_s a
-	left join prev_owlexpressionrefset_s b
-	on a.id = b.id
-	where
-	a.active = 0
-	and a.owlexpression <> b.owlexpression;
-	commit;
+	
