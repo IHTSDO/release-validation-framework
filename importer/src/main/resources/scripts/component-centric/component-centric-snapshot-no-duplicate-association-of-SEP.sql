d3888bfffb9d1bda7179e4e49d54be5611e0acf3
Quyen Ly <quocquyenit@gmail.com>
2020-03-18 11:09:46 +0700

    MAINT-1114 Filter only active concepts

--- importer/src/main/resources/scripts/component-centric/component-centric-snapshot-no-duplicate-association-of-SEP.sql
+++ importer/src/main/resources/scripts/component-centric/component-centric-snapshot-no-duplicate-association-of-SEP.sql
@@ -10,9 +10,10 @@
 		'<ASSERTIONUUID>',
 		a.referencedcomponentid,
 		concat('Active structure concept: id=',a.referencedcomponentid, ' is being referenced by multiple ', if(a.refsetid = '734138000', 'Entire', 'Part') , ' concepts id=', group_concat(a.targetcomponentid), '.')
-	from curr_associationrefset_s a
+	from curr_associationrefset_s a left join curr_concept_s b on a.targetcomponentid = b.id
 	where a.refsetid in ('734138000','734139008')
 	    and a.active = '1'
+	    and b.active = '1'
     group by a.referencedcomponentid, a.refsetid
     having count(a.targetcomponentid) > 1;
 	
