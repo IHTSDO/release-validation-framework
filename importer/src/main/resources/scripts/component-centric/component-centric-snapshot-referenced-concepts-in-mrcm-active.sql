905ab78a7e0a5480c6b9dee839a123e02bdc8bee
Quyen Ly <quocquyenit@gmail.com>
2020-03-18 10:10:30 +0700

    MAINT-1219 Validate Snapshot file

--- importer/src/main/resources/scripts/component-centric/component-centric-snapshot-referenced-concepts-in-mrcm-active.sql
+++ importer/src/main/resources/scripts/component-centric/component-centric-snapshot-referenced-concepts-in-mrcm-active.sql
@@ -22,7 +22,7 @@ component-centric-snapshot-referenced-concepts-in-mrcm-active.sql
 		'<ASSERTIONUUID>',
 		a.referencedcomponentid,
 		concat('Reference component id:',a.referencedcomponentid, ' in MRCM Attribute Domain refset must be active.')
-	from curr_mrcmAttributeDomainRefset_d a left join tmp_active_concepts b on a.referencedcomponentid = b.conceptid
+	from curr_mrcmAttributeDomainRefset_s a left join tmp_active_concepts b on a.referencedcomponentid = b.conceptid
 	where a.active = '1'
         and b.conceptid is null;
 
@@ -32,7 +32,7 @@ component-centric-snapshot-referenced-concepts-in-mrcm-active.sql
         '<ASSERTIONUUID>',
         a.referencedcomponentid,
         concat('Reference component id:',a.referencedcomponentid, ' in MRCM Attribute Range refset must be active.')
-    from curr_mrcmAttributeRangeRefset_d a left join tmp_active_concepts b on a.referencedcomponentid = b.conceptid
+    from curr_mrcmAttributeRangeRefset_s a left join tmp_active_concepts b on a.referencedcomponentid = b.conceptid
     where a.active = '1'
         and b.conceptid is null;
 
@@ -42,7 +42,7 @@ component-centric-snapshot-referenced-concepts-in-mrcm-active.sql
         '<ASSERTIONUUID>',
         a.referencedcomponentid,
         concat('Reference component id:',a.referencedcomponentid, ' in MRCM  Domain refset must be active.')
-    from curr_mrcmDomainRefset_d a left join tmp_active_concepts b on a.referencedcomponentid = b.conceptid
+    from curr_mrcmDomainRefset_s a left join tmp_active_concepts b on a.referencedcomponentid = b.conceptid
     where a.active = '1'
         and b.conceptid is null;
 
@@ -52,7 +52,7 @@ component-centric-snapshot-referenced-concepts-in-mrcm-active.sql
         '<ASSERTIONUUID>',
         a.referencedcomponentid,
         concat('Reference component id:',a.referencedcomponentid, ' in MRCM module scope refset must be active.')
-    from curr_mrcmModuleScopeRefset_d a left join tmp_active_concepts b on a.referencedcomponentid = b.conceptid
+    from curr_mrcmModuleScopeRefset_s a left join tmp_active_concepts b on a.referencedcomponentid = b.conceptid
     where a.active = '1'
         and b.conceptid is null;
 
