05e2845352d87384e24ffaeb5c41d39eecf3c985
Quyen Ly <quocquyenit@gmail.com>
2020-03-20 16:53:28 +0700

    MAINT-1219 Check if referenced conept exists

--- importer/src/main/resources/scripts/component-centric/component-centric-snapshot-referenced-concepts-in-mrcm-active.sql
+++ importer/src/main/resources/scripts/component-centric/component-centric-snapshot-referenced-concepts-in-mrcm-active.sql
@@ -15,7 +15,7 @@ component-centric-snapshot-referenced-concepts-in-mrcm-active.sql
 		concat('Reference component id:',a.referencedcomponentid, ' in MRCM Attribute Domain refset must be active.')
 	from curr_mrcmattributedomainrefset_s a left join curr_concept_s b on a.referencedcomponentid = b.id
 	where a.active = '1'
-        and b.active = '0';
+        and (b.active = '0' or b.id is null);
 
     insert into qa_result (runid, assertionuuid, concept_id, details)
     select
@@ -25,7 +25,7 @@ component-centric-snapshot-referenced-concepts-in-mrcm-active.sql
         concat('Reference component id:',a.referencedcomponentid, ' in MRCM Attribute Range refset must be active.')
     from curr_mrcmattributerangerefset_s a left join curr_concept_s b on a.referencedcomponentid = b.id
     where a.active = '1'
-        and b.active = '0';
+        and (b.active = '0' or b.id is null);
 
     insert into qa_result (runid, assertionuuid, concept_id, details)
     select
@@ -35,7 +35,7 @@ component-centric-snapshot-referenced-concepts-in-mrcm-active.sql
         concat('Reference component id:',a.referencedcomponentid, ' in MRCM  Domain refset must be active.')
     from curr_mrcmdomainrefset_s a left join curr_concept_s b on a.referencedcomponentid = b.id
     where a.active = '1'
-        and b.active = '0';
+        and (b.active = '0' or b.id is null);
 
     insert into qa_result (runid, assertionuuid, concept_id, details)
     select
@@ -45,4 +45,4 @@ component-centric-snapshot-referenced-concepts-in-mrcm-active.sql
         concat('Reference component id:',a.referencedcomponentid, ' in MRCM module scope refset must be active.')
     from curr_mrcmmodulescoperefset_s a left join curr_concept_s b on a.referencedcomponentid = b.id
     where a.active = '1'
-        and b.active = '0';
+        and (b.active = '0' or b.id is null);
