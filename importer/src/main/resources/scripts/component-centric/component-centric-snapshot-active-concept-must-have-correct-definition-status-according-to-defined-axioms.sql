cae51d0d96f89d2ca047c3cf939fb569e2fbd7fd
Quyen Ly <quocquyenit@gmail.com>
2020-09-04 10:45:39 +0700

    VAL-99 Corrected logic for assertion: Concepts having wrong definition status according to defined axioms

--- importer/src/main/resources/scripts/component-centric/component-centric-snapshot-active-concept-must-have-correct-definition-status-according-to-defined-axioms.sql
+++ importer/src/main/resources/scripts/component-centric/component-centric-snapshot-active-concept-must-have-correct-definition-status-according-to-defined-axioms.sql
@@ -9,12 +9,9 @@ Active concept must have correct definition status according to defined axioms
 		'<ASSERTIONUUID>',
 		a.id,
 		concat('Active concept ', a.id, ' is having incorrect definition status according to defined axioms')
-	from curr_concept_s a left join curr_owlexpressionrefset_s b
-	on a.id = b.referencedcomponentid
-	where a.active=1
-	and b.active=1
-	and b.refsetid='733073007'
-	and ((a.definitionstatusid='900000000000074008' and (b.owlexpression not like '%SubClassOf%' or b.owlexpression like '%EquivalentClasses%'))   /* primitive */
-	     or (a.definitionstatusid='900000000000073002' and b.owlexpression not like '%EquivalentClasses%') /* defined */
-	     );
-	
+	from curr_concept_s a
+       left join (select distinct referencedcomponentid from curr_owlexpressionrefset_s where active = '1' and owlexpression like '%EquivalentClasses%') b
+       on a.id = b.referencedcomponentid
+    where a.active = '1'
+    and a.definitionstatusid = '900000000000074008' /* primitive */
+    and b.referencedcomponentid is not null;
