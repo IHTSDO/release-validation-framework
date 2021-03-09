1ddad20c869a02643774c217c4e1f29c35830dfa
Quyen Ly <quocquyenit@gmail.com>
2021-03-09 13:55:17 +0700

    VAL-117 Updated assertion to filter only active concepts

--- importer/src/main/resources/scripts/file-centric/file-centric-snapshot-description-term-duplicated.sql
+++ importer/src/main/resources/scripts/file-centric/file-centric-snapshot-description-term-duplicated.sql
@@ -12,8 +12,9 @@
 		'<ASSERTIONUUID>',
 		a.conceptid,
 		concat('DESCRIPTION : ids =', GROUP_CONCAT(DISTINCT a.id ORDER BY a.id ASC SEPARATOR ', '), ' : Duplicated terms in DESCRIPTION snapshot.')
-	from curr_description_s a
-	where a.active = '1' and casesignificanceid = 900000000000448009
+	from curr_description_s a left join curr_concept_s b
+	on a.conceptid = b.id
+	where a.active = '1' and b.active = '1' and a.casesignificanceid = 900000000000448009
 	group by LOWER(a.term), a.conceptid, a.typeid, a.languagecode
 	having COUNT(*) > 1;
 	commit;
