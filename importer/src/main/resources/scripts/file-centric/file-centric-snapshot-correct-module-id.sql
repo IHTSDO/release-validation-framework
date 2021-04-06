0046b2c5a6bebda54911d62e8b94120cf8a5ff11
Quyen Ly <quocquyenit@gmail.com>
2021-04-06 10:46:16 +0700

    VAL-62 Applied the assertion to only Derivative and extension products

--- importer/src/main/resources/scripts/file-centric/file-centric-snapshot-correct-module-id.sql
+++ importer/src/main/resources/scripts/file-centric/file-centric-snapshot-correct-module-id.sql
@@ -11,12 +11,10 @@ create table module_id(
 moduleid bigint(20) not null
 ) engine=myisam default charset=utf8;
 
+insert into module_id(moduleid) values(900000000000012004); /* SNOMED CT model component module */
+
 insert into module_id
 select distinct moduleid from curr_moduledependencyrefset_s
 where active = 1;
 
-insert into module_id(moduleid) values(900000000000207008);
-insert into module_id(moduleid) values(900000000000012004);
-
 call validate_module_id('<PROSPECTIVE>',<RUNID>,'<ASSERTIONUUID>');
-	
