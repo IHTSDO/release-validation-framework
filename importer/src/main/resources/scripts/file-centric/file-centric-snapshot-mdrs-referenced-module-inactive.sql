ce1f98b5307eafe7610a23ce7bcdad47d8ed4d02
Attila Edelenyi <attila.edelenyi@csiro.au>
2021-04-13 14:53:17 +1000

    Referenced module in the MRDS is inactive

---
+++ importer/src/main/resources/scripts/file-centric/file-centric-snapshot-mdrs-referenced-module-inactive.sql
@@ -0,0 +1,17 @@
+
+/********************************************************************************
+	file-centric-snapshot-mdrs-referenced-module-inactive.sql
+
+	Assertion:
+	Referenced module in the MDRS is inactive
+********************************************************************************/
+
+	insert into qa_result (runid, assertionuuid, concept_id, details)
+	select
+		<RUNID>,
+		'<ASSERTIONUUID>',
+		a.referencedcomponentid,
+		concat('Referenced module ', a.referencedcomponentid, ' in module dependency refset is inactive.') 
+	from moduledependencyrefset_s a
+	left join concept_s c on a.referencedcomponentid = c.id
+	where c.active = 0;
