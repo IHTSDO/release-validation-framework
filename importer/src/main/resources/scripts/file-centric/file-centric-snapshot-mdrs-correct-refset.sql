5d98f80e2e2f9957578d69998d8aee48e6f9052c
Attila Edelenyi <attila.edelenyi@csiro.au>
2021-04-16 08:53:11 +1000

    Fix test

--- importer/src/main/resources/scripts/file-centric/file-centric-snapshot-mdrs-correct-refset.sql
+++ importer/src/main/resources/scripts/file-centric/file-centric-snapshot-mdrs-correct-refset.sql
@@ -4,11 +4,11 @@ file-centric-snapshot-mdrs-correct-refset.sql
         "All module dependency rows should be in the module dependency refset"
 ********************************************************************************/
 
-    insert into qa_result (runid, assertionuuid, concept_id, details)
-    select
-        <RUNID>,
-        '<ASSERTIONUUID>',
-        a.referencedcomponentid,
-        concat('Dependency from: ', a.moduleid, ' - ', a.sourceeffectivetime, ' to: ', b.referencedcomponentid, ' - ', b.targeteffectivetime, ' is declared in the incorrect refset ', a.refsetId)
-    from curr_moduledependencyrefset_s a
-        where a.active = '1' and a.refsetId != '900000000000534007' 
+	insert into qa_result (runid, assertionuuid, concept_id, details)
+	select
+		<RUNID>,
+		'<ASSERTIONUUID>',
+		a.referencedcomponentid,
+		concat('Dependency from: ', a.moduleid, ' - ', a.sourceeffectivetime, ' to: ', a.referencedcomponentid, ' - ', b.targeteffectivetime, ' is declared in the incorrect refset ', a.refsetId)
+	from curr_moduledependencyrefset_s a
+		where a.active = '1' and a.refsetId != '900000000000534007';
