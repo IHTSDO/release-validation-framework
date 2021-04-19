c3860ac3eeeff57103bd49765a5c55a801b85a5b
Attila Edelenyi <attila.edelenyi@csiro.au>
2021-04-19 13:32:59 +1000

    Root concept (concept that has no isa) shouldn't really have non-isa relationships

--- importer/src/main/resources/scripts/file-centric/file-centric-snapshot-root-concept-has-non-isa-relationships.sql
+++ importer/src/main/resources/scripts/file-centric/file-centric-snapshot-root-concept-has-non-isa-relationships.sql
@@ -11,15 +11,15 @@ insert into qa_result (runid, assertionuuid, concept_id, details)
 	select distinct
 		<RUNID>,
 		'<ASSERTIONUUID>',
-		r.destinationid,
-		concat('ROOT CONCEPT: ',r.destinationid, 'has non-isa relationship: ', r.typeid)
+		r.sourceid,
+		concat('ROOT CONCEPT: ',r.sourceid, ' has non-isa relationship id=', r.id, ' with typeid=', r.typeid)
 		from curr_relationship_s r 
-		where r.destinationid in 
+		where r.sourceid in 
 			(select c.id from curr_concept_s c
 				where not exists 
 					(select r.id from curr_relationship_s s 
 						where s.sourceid = c.id
-							and s.active = 1)
+							and s.active = 1
+							and s.typeid = 116680003)
 				and c.active = 1)
-			and r.active = 1
-			and r.typeid != 116680003;
+			and r.active = 1;
