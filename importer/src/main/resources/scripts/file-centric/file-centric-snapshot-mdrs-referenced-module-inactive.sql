4de295acb218aabceb93ff994aab616e9b546ffa
Attila Edelenyi <attila.edelenyi@csiro.au>
2021-04-22 12:29:21 +1000

    Add check for source module ids that are inactive

--- importer/src/main/resources/scripts/file-centric/file-centric-snapshot-mdrs-referenced-module-inactive.sql
+++ importer/src/main/resources/scripts/file-centric/file-centric-snapshot-mdrs-referenced-module-inactive.sql
@@ -16,3 +16,14 @@
 	left join curr_concept_s c on a.referencedcomponentid = c.id
 	where c.active = 0
 		and a.active = 1;
+
+	insert into qa_result (runid, assertionuuid, concept_id, details)
+	select
+		<RUNID>,
+		'<ASSERTIONUUID>',
+		a.moduleid,
+		concat('Sourced module ', a.moduleid, ' in module dependency refset is inactive.') 
+	from curr_moduledependencyrefset_s a
+	left join curr_concept_s c on a.moduleid = c.id
+	where c.active = 0
+		and a.active = 1;
