449799b60a02d92ea1b9f65462d7c8312b31ac02
Quyen Ly <quocquyenit@gmail.com>
2021-07-26 12:52:22 +0700

    VAL-124 Specified assertion groups(INT, US) to asserstion: Reference component id must be valid in extended map refset

--- importer/src/main/resources/scripts/component-centric/component-centric-snapshot-referencedComponentID-of-refset-member-must-be-valid.sql
+++ importer/src/main/resources/scripts/component-centric/component-centric-snapshot-referencedComponentID-of-refset-member-must-be-valid.sql
@@ -46,18 +46,6 @@ component-centric-snapshot-referencedComponentID-of-refeset-member-must-be-valid
         <RUNID>,
         '<ASSERTIONUUID>',
         a.referencedcomponentid,
-        concat('Reference component id:',a.referencedcomponentid,' for refset id: ', a.refsetid, ' in extended map refset must be valid.'),
-        a.id,
-        'curr_extendedmaprefset_s'
-    from curr_extendedmaprefset_s a left join curr_concept_s b on a.referencedcomponentid = b.id
-    where a.active = '1' and (b.active = '0' or b.id is null);
-
-
-    insert into qa_result (runid, assertionuuid, concept_id, details, component_id, table_name)
-    select
-        <RUNID>,
-        '<ASSERTIONUUID>',
-        a.referencedcomponentid,
         concat('Reference component id:',a.referencedcomponentid,' for refset id: ', a.refsetid, ' in map correlation origin refset must be valid.'),
         a.id,
         'curr_mapcorrelationoriginrefset_s'
