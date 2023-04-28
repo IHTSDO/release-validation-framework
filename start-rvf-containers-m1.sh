e8bb6de36f00d026aa05238601534a342fee26fb
lukeswindale <luke@swinmail.com>
2023-04-28 14:44:56 +1000

    Revert "Added some build variations for M1 chip Mac"
    This reverts commit a51621700d522acee61d3b903df8676067ca2d49.

--- start-rvf-containers-m1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# This script starts the RVF containers for the M1 processor.
-
-set -e
-
-./stop-rvf-containers-m1.sh
-docker-compose -f docker-compose.yml -f docker-compose.m1.yml up -d
--- stop-rvf-containers-m1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# This script stops the RVF containers for the M1 processor.
-
-set -e
-
-docker-compose -f docker-compose.yml -f docker-compose.m1.yml down
