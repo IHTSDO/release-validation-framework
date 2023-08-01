e6f3684e90f8f2a67bec50500cf355b55a95294a
lukeswindale <luke@swinmail.com>
2023-08-01 21:31:50 +1000

    Increased the term size for descriptions to 2048

--- src/main/resources/sql/create-resource-tables.sql
+++ src/main/resources/sql/create-resource-tables.sql
@@ -1,7 +1,7 @@
 drop table if exists res_us_gb_terms;
 create table res_us_gb_terms (
-	us_term	varchar(255),
-	gb_term	varchar(255)
+	us_term VARCHAR(2048),
+	gb_term VARCHAR(2048)
 )engine=myisam default charset=utf8;
 create index idx_res_usTermMap on res_us_gb_terms(us_term);
 create index idx_res_gbTermMap on res_us_gb_terms(gb_term);
@@ -10,7 +10,7 @@ create index idx_res_gbTermMap on res_us_gb_terms(gb_term);
 drop table if exists res_casesensitiveTerm;
 
 create table res_casesensitiveTerm(
-   casesensitiveTerm VARCHAR(255) not null
+   casesensitiveTerm VARCHAR(2048) not null
 )engine=myisam default charset=utf8;
 create index idx_casesensitiveTerm on res_casesensitiveTerm(casesensitiveTerm);
 
