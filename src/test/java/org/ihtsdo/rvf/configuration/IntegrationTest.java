e9feb2429ced0e4bbcb6c052a334e9e8b40f5655
Darryn McGaw <dmcg@ihtsdo.org>
2026-08-26 14:55:47 +0100

    VAL-440 Start MySQL container once

--- src/test/java/org/ihtsdo/rvf/configuration/IntegrationTest.java
+++ src/test/java/org/ihtsdo/rvf/configuration/IntegrationTest.java
@@ -8,32 +8,30 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.ResultActions;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.mysql.MySQLContainer;
 
 import javax.sql.DataSource;
 
 import java.sql.Connection;
 
-@Testcontainers
 @SpringBootTest(classes = {App.class})
 @PropertySource("classpath:/application-test.properties")
 @ActiveProfiles("test")
 @WebAppConfiguration
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public abstract class IntegrationTest {
 	private static final Logger LOGGER = LoggerFactory.getLogger(IntegrationTest.class);
 	protected static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
-	@Container
-	public static MySQLContainer mySQLContainer = new TestMySQLContainer();
+	public static final MySQLContainer mySQLContainer = new TestMySQLContainer();
+
+	static {
+		mySQLContainer.start();
+	}
 
 	@Autowired
 	private DataSource dataSource;
